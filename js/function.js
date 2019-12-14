0396621ceddfe3257d62b450ee0e9fc7a95851eb
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-14 14:13:25 +0330

    new functions added

--- js/function.js
+++ js/function.js
@@ -1,19 +1,55 @@
 //functions 
 
+//init
+let init = function () {
+    
+    dataset[1].forEach(function(item){
+        let ratesArray = giveRates(item.item_id);
+        //initialize basic reputation
+        let rep = getMean(ratesArray);
+        item['rep'] = rep;
+        //initialize standard deviation
+        let sd = getSD(ratesArray);
+        item['sd'] = sd;
+    });
+    calculateRateObjectivity();
+
+}
+
+// Arithmetic mean
+let getMean = function (data) {
+    return data.reduce(function (a, b) {
+        return Number(a) + Number(b);
+    }) / data.length;
+};
+
+// Standard deviation
+let getSD = function (data) {
+    let m = getMean(data);
+    return Math.sqrt(data.reduce(function (sq, n) {
+            return sq + Math.pow(n - m, 2);
+        }, 0) / (data.length - 1));
+};
 
 //giveUsers
 //input: an specific item (m)
 //process :
 //return  all users give rate to item m
-
-
-
+let giveUsers = function(m){
+    const usersRatesOnProduct = [];
+    let properItem = dataset[1].find(function(item) {
+        return item.item_id === m;
+    });
+    properItem.rated.forEach(function (item) {
+        usersRatesOnProduct.push(item.user_id);
+    });
+    return usersRatesOnProduct;
+}
 
 //giveRates 
 //input: an specific item (m) --> id of item : number
 //process
 //output: returns all rates to item (m) in array
-
 let giveRates = function(productId){  
     let ratesOnProduct = [];  
     let properItem = dataset[1].find(function(product) {
@@ -24,7 +60,6 @@ let giveRates = function(productId){
     });
     return ratesOnProduct;
 }
-
 
 //giveUsersRates()
 //input: an specific user (u) --> u = id of user like : e9343-3434
@@ -38,15 +73,16 @@ const giveUsersRates = function (u) {
     console.log(properUser.rating.length);
 }
 
+//calculateRateObjectivity
+//input (rep + sd + rate)
+//process : Math.abs((rate - rep)/sd)
+//output : objectivity of rate
 
-//calculateBsicReputation
-//input an array
-//process : (sum)/numner of items
-// output : return the medium
-let calculateBsicReputation = function(arrayOfRates){
-    let sum = 0;
-    arrayOfRates.forEach(function(rate) {
-        sum += rate;
-    })
-    return (sum / arrayOfRates.length);
+let calculateRateObjectivity = function() {
+    dataset[1].forEach(function (item) {
+        item.rated.forEach(function (subItem) {
+            let or = Math.abs((subItem.rate - item.rep) / item.sd);
+            subItem['or'] = or;
+        });
+    });
 }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 let dataset;
 
-let xhr = new XMLHttpRequest() ;
-xhr.open('GET' , './js/dataset.json', true);
-xhr.send();
-xhr.onload = function () {
-    dataset = JSON.parse(xhr.responseText);
+
+if(!(localStorage.getItem('dataset'))){
+    let xhr = new XMLHttpRequest() ;
+    xhr.open('GET' , './js/dataset.json', true);
+    xhr.send();
+    xhr.onload = function () {
+        dataset = JSON.parse(xhr.responseText);
+        localStorage.setItem('dataset' , JSON.stringify(dataset));
+    }
+}else{
+    dataset = JSON.parse(localStorage.getItem('dataset'));
 }
+
 
 setTimeout(function() {
     console.log(dataset);
 
-    giveUsersRates(24);
-    console.log(giveRates(1));
-    console.log(calculateBsicReputation(giveRates(1)));
+    init();
+
+    // giveUsersRates(24);
+    // console.log(giveRates(1));
+    // console.log(getMean(giveRates(1)));
+    // console.log(getSD(giveRates(1)));
+    // console.log(giveUsers(0));
 
 } ,1000);
