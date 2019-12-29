ec0a761bfde907db3bd6e156030466018aca30be
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-29 15:40:56 +0330

    box plot now is available now

--- js/math.js
+++ js/math.js
@@ -25,24 +25,55 @@ let arraySum = function (arrayOfNumbers) {
 }
 
 //Finding Median
-function median(values){
+function calculateMedian(values){
     if(values.length ===0) return 0;
   
     values.sort(function(a,b){
       return a-b;
     });
     console.log(values);
-    var half = Math.floor(values.length / 2);
+    let half = Math.floor(values.length / 2);
   
     if (values.length % 2)
-      return values[half];
-  
+    return values[half];
+
     return (values[half - 1] + values[half]) / 2.0;
-  }
-let randomArr = [2,1,3,4,5,4,5,2,3,4,1.5 ,5]
-let medianVal = median(randomArr);
-if(!(randomArr.length % 2)){
-    randomArr.splice(randomArr.length/2 , 0 , medianVal);
 }
-console.log(medianVal);
-console.log(randomArr);
+
+let setBoxPlot = function(arr) {
+    let firstHalf , secondHalf , q1 , q3 , IQR;
+    //calculate median
+    const median = calculateMedian(arr);
+    if(!(arr.length % 2)){
+        arr.splice(arr.length/2 , 0 , median);
+    }
+    //split arry to get 25th & 75th percentile
+    firstHalf = arr.slice(-arr.length , -arr.length/2 -1);
+    secondHalf = arr.slice(-arr.length/2);
+    //median of 25th percentile
+    q1 = calculateMedian(firstHalf);
+    //median of 75th percentile
+    q3 = calculateMedian(secondHalf);
+    //put the median if its a new value
+    if(!(firstHalf.length % 2)){
+        firstHalf.splice(firstHalf.length/2 , 0 , q1);
+    }
+    if(!(secondHalf.length % 2)){
+        secondHalf.splice(secondHalf.length/2 , 0 , q3);
+    }
+    //Caculate IQR : The Proper Values for Ratigs are between q1 and q3 plus 1 for median
+    IQR = Math.ceil((firstHalf.length) / 2) + Math.ceil((secondHalf.length /2)) + 1;
+    //Final Array : leftSide + median + rightSide
+    let wisker = firstHalf.concat(median).concat(secondHalf);
+    return {
+        wisker,
+        median,
+        q1,
+        q3,
+        IQR
+    };
+}
+
+//Tesing
+let value = setBoxPlot([1.4 , 1.1 , 1.5 , 2 , 5 , 0.8 , 0.8, 0.3 , 0.9 , 1.3 , 1.3 , 2]);
+
